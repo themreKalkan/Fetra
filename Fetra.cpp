dd5d7645ed6bef24d92661d837ec601db768080a
Emre Kalkan <emre11klk@gmail.com>
2025-02-18 15:07:17 +0300

    Add files via upload

--- Fetra.cpp
+++ Fetra.cpp
@@ -11,6 +11,7 @@ Fetra::Fetra() :
     _calibrationMax(nullptr),
     _dpr(3) ,
     _lineThickness(20),
+    _lastLinePos(0),
     _distBetweenSens(10){}
 
 // Destructor implementation
@@ -30,10 +31,10 @@ void Fetra::sensorPins(int pins[], int sensorCount) {
 
     // Allocate new arrays
     _sensorCount = sensorCount;
-    _analogPins = new int[_sensorCount];
-    _analogReads = new int[_sensorCount];
-    _calibrationMin = new int[_sensorCount];
-    _calibrationMax = new int[_sensorCount];
+    _analogPins = new uint8_t[_sensorCount];
+    _analogReads = new uint16_t[_sensorCount];
+    _calibrationMin = new uint16_t[_sensorCount];
+    _calibrationMax = new uint16_t[_sensorCount];
 
     lineBlackColor = true;
 
@@ -72,10 +73,10 @@ void Fetra::readAnalogs() {
     for(int i = 0; i < _sensorCount; i++) {
         _analogReads[i] = analogRead(_analogPins[i]);
     }
-    return _analogReads;
 }
 
 int* Fetra::getAnalogReads() {
+    readAnalogs();
     return _analogReads; // Return the pointer to the analog reads array
 }
 
@@ -94,6 +95,30 @@ void Fetra::calibrate(int repeat) {
     }
 }
 
+void Fetra::calibrateOnce() {
+        readAnalogs();
+        for(int s = 0; s < _sensorCount; s++) {
+            if(_analogReads[s] < _calibrationMin[s]) {
+                _calibrationMin[s] = _analogReads[s];
+            }
+            if(_analogReads[s] > _calibrationMax[s]) {
+                _calibrationMax[s] = _analogReads[s];
+            }
+        }
+    
+}
+
+void Fetra::calibrateForAll(int maxV,int minV) {
+    //readAnalogs();
+    for(int s = 0; s < _sensorCount; s++) {
+            _calibrationMin[s] = minV;
+       
+            _calibrationMax[s] = maxV;
+        
+    }
+
+}
+
 void Fetra::calibratedAnalog() {
     readAnalogs();
     if(lineBlackColor == false){invert_analog();}
@@ -109,9 +134,12 @@ void Fetra::calibratedAnalog() {
     }
 }
 
-uint16_t Fetra::linePosition() {
+uint16_t Fetra::dashedLinePosition() {
     uint32_t total = 0;
     uint32_t lineSum = 0;
+
+    uint8_t densL = lineDensAdvanced();
+
     
     for(uint8_t s = 0; s < _dpr; s++) {
         calibratedAnalog();
@@ -127,8 +155,51 @@ uint16_t Fetra::linePosition() {
         total += iterationTotal;
     }
     
-    if(total == 0) return -1;
-    return lineSum / (total);
+    
+    if(densL > 15){
+        _lastLinePos = lineSum / (total);
+        return _lastLinePos;
+    }
+    else{
+        
+            if(_lastLinePos > ((_sensorCount+1)*500)){
+                return (_sensorCount-1)*1000;
+            }
+            else if(_lastLinePos < ((_sensorCount-3)*500)){return 0;}
+            else{return 3500;}
+    }
+    
+
+    
+}
+
+uint16_t Fetra::linePosition() {
+    uint32_t total = 0;
+    uint32_t lineSum = 0;
+
+    
+    for(uint8_t s = 0; s < _dpr; s++) {
+        calibratedAnalog();
+        uint32_t iterationSum = 0;
+        uint32_t iterationTotal = 0;
+        
+        for(uint8_t i = 0; i < _sensorCount; i++) {
+            iterationSum += (uint32_t)_analogReads[i] * i * 1000;
+            iterationTotal += _analogReads[i];
+        }
+        
+        lineSum += iterationSum;
+        total += iterationTotal;
+    }
+    
+    if(total == 0){
+        return _lastLinePos;
+    }
+    _lastLinePos = lineSum / (total);
+        return _lastLinePos;
+    
+
+    
 }
 
 
@@ -150,7 +221,7 @@ uint16_t Fetra::lineDensity(){
 
 uint16_t Fetra::lineDensAdvanced(){
     //calibratedAnalog();
-    int mainDistance = (_sensorCount+2)*_distBetweenSens;
+    //int mainDistance = (_sensorCount+2)*_distBetweenSens;
     int lineSum = 0;
     for(int i = 0;i < _sensorCount;i++){
         lineSum += (_analogReads[i]*_distBetweenSens)/1000;
@@ -160,6 +231,26 @@ uint16_t Fetra::lineDensAdvanced(){
 
 int8_t Fetra::lineAngle(){
     //calibratedAnalog();
+    int midLine = (_sensorCount-1)*500;
+    bool passMod = false;
+    if(_sensorCount%2 == 1){
+        passMod = true;
+    }
+    
+    for(int i = 0;i<_sensorCount;i++){
+        if(passMod && i == _sensorCount/2){continue;}
+        if(i<_sensorCount/2){midLine -= _analogReads[i];}
+        else{midLine += _analogReads[i];}
+    }
+    midLine = map(midLine,500,6500,-90,90);
+    //    midLine = map(midLine,0,7000,-90,90);
+
+    return midLine;
+}
+
+int8_t Fetra::lineAnglePure(){
+    //calibratedAnalog();
+    readAnalogs();
     int midLine = (_sensorCount-1)*500;
     bool passMod = false;
     if(_sensorCount%2 == 1){
--- Fetra.h
+++ Fetra.h
@@ -12,28 +12,34 @@ public:
     void dataPerRead(int dpr);
     void setLineThick(int lt);
     void setSensBtwDist(int sbd);
-    void readAnalogs();
     void calibrate(int repeat);
+    void calibrateForAll(int maxV,int minV);
+    void calibrateOnce();
     uint16_t linePosition();  
+    uint16_t dashedLinePosition();  
     int* getAnalogReads(); 
     void calibratedAnalog(); 
     uint16_t lineDensity();
     uint16_t lineDensAdvanced();
     int8_t lineAngle();
+    int8_t lineAnglePure();
     bool lineBlackColor;
     void setLineColorBlack();
     void setLineColorWhite();
+    
 
 
 private:
-    int _sensorCount;    
-    int* _analogPins;
-    int* _analogReads;
-    int* _calibrationMin;
-    int* _calibrationMax;
-    int _dpr;
-    int _lineThickness;
-    int _distBetweenSens;
+    uint8_t _sensorCount;    
+    uint8_t* _analogPins;
+    uint16_t* _analogReads;
+    uint16_t* _calibrationMin;
+    uint16_t* _calibrationMax;
+    uint8_t _dpr;
+    uint8_t _lineThickness;
+    uint8_t _distBetweenSens;
+    uint16_t _lastLinePos;
+    void readAnalogs();
     
     void invert_analog();
      // Changed return type to void
